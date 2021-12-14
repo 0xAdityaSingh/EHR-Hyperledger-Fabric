c70496f31759f27855208c916718e81b38d991c1
GoodwillHunter <utkarsh18114@iiitd.ac.in>
2021-12-14 17:25:43 +0530

    register smart contract added

--- fabcar/apiserver/api_query.js
+++ fabcar/apiserver/api_query.js
@@ -143,7 +143,7 @@ const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizatio
         console.log(`Wallet path: ${walletPath}`);
 
         // Check to see if we've already enrolled the user.
-        const identity = await wallet.get('Doctor1');
+        const identity = await wallet.get(req.body.userId);
         if (!identity) {
             console.log('An identity for the user "Doctor1" does not exist in the wallet');
             console.log('Run the registerUser.js application before retrying');
@@ -151,7 +151,7 @@ const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizatio
         }
   // Create a new gateway for connecting to our peer node.
         const gateway = new Gateway();
-        await gateway.connect(ccp, { wallet, identity: 'Doctor1', discovery: { enabled: true, asLocalhost: true } });
+        await gateway.connect(ccp, { wallet, identity: req.body.userId, discovery: { enabled: true, asLocalhost: true } });
 
         // Get the network (channel) our contract is deployed to.
         const network = await gateway.getNetwork('mychannel');
@@ -174,44 +174,44 @@ const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizatio
 
 
 
-app.put('/api/changeowner/:car_index', async function (req, res) {
-    try {
-const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
-        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
-// Create a new file system based wallet for managing identities.
-        const walletPath = path.join(process.cwd(), 'wallet');
-        const wallet = await Wallets.newFileSystemWallet(walletPath);
-        console.log(`Wallet path: ${walletPath}`);
+// app.put('/api/changeowner/:car_index', async function (req, res) {
+//     try {
+// const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+//         const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+// // Create a new file system based wallet for managing identities.
+//         const walletPath = path.join(process.cwd(), 'wallet');
+//         const wallet = await Wallets.newFileSystemWallet(walletPath);
+//         console.log(`Wallet path: ${walletPath}`);
 
-        // Check to see if we've already enrolled the user.
-        const identity = await wallet.get('appUser');
-        if (!identity) {
-            console.log('An identity for the user "appUser" does not exist in the wallet');
-            console.log('Run the registerUser.js application before retrying');
-            return;
-        }
-  // Create a new gateway for connecting to our peer node.
-        const gateway = new Gateway();
-        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+//         // Check to see if we've already enrolled the user.
+//         const identity = await wallet.get('appUser');
+//         if (!identity) {
+//             console.log('An identity for the user "appUser" does not exist in the wallet');
+//             console.log('Run the registerUser.js application before retrying');
+//             return;
+//         }
+//   // Create a new gateway for connecting to our peer node.
+//         const gateway = new Gateway();
+//         await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
 
-        // Get the network (channel) our contract is deployed to.
-        const network = await gateway.getNetwork('mychannel'); 
+//         // Get the network (channel) our contract is deployed to.
+//         const network = await gateway.getNetwork('mychannel'); 
 
-        // Get the contract from the network.
-        const contract = network.getContract('fabcar');
-// Submit the specified transaction.
-        // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
-        // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
-        await contract.submitTransaction('changeCarOwner', req.params.car_index, req.body.owner);
-        console.log('Transaction has been submitted');
-        res.send('Transaction has been submitted');
-// Disconnect from the gateway.
-        await gateway.disconnect();
-} catch (error) {
-        console.error(`Failed to submit transaction: ${error}`);
-        process.exit(1);
-    } 
-})
+//         // Get the contract from the network.
+//         const contract = network.getContract('fabcar');
+// // Submit the specified transaction.
+//         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
+//         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
+//         await contract.submitTransaction('changeCarOwner', req.params.car_index, req.body.owner);
+//         console.log('Transaction has been submitted');
+//         res.send('Transaction has been submitted');
+// // Disconnect from the gateway.
+//         await gateway.disconnect();
+// } catch (error) {
+//         console.error(`Failed to submit transaction: ${error}`);
+//         process.exit(1);
+//     } 
+// })
 
 app.post('/api/grant', async function (req, res)  {
     // user-id, record-id, iser-id-2
@@ -300,5 +300,76 @@ app.post('/api/revoke', async function (req, res)  {
         process.exit(1);
     }
 })
+
+
+app.post('/api/register', async function (req, res)  {
+    try {
+        // load the network configuration
+        const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+        // Create a new CA client for interacting with the CA.
+        const caURL = ccp.certificateAuthorities['ca.org1.example.com'].url;
+        const ca = new FabricCAServices(caURL);
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userIdentity = await wallet.get(req.body.userId2);
+        if (userIdentity) {
+            console.log('An identity for the user "doctor1" already exists in the wallet');
+            res.status(200).json({response: 'An identity for the user already exists in the wallet'});
+            return;
+        }
+
+        // Check to see if we've already enrolled the admin user.
+        const adminIdentity = await wallet.get(req.body.userId);
+        if (!adminIdentity) {
+            console.log('An identity for the admin user "admin" does not exist in the wallet');
+            console.log('Run the enrollAdmin.js application before retrying');
+            res.status(404).json({error: 'An identity for the admin user "admin" does not exist in the wallet'});
+            return;
+        }
+
+        // build a user object for authenticating with the CA
+        const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
+        const adminUser = await provider.getUserContext(adminIdentity, req.body.userId);
+
+        // Register the user, enroll the user, and import the new identity into the wallet.
+        const secret = await ca.register({
+            affiliation: 'org1.department1',
+            enrollmentID: req.body.userId2,
+            role: 'client',
+            attrs: [
+                {"name": "hf.Registrar.Roles", "value": req.body.role}
+            ],
+        }, adminUser);
+        const enrollment = await ca.enroll({
+            enrollmentID: 'doctor1',
+            enrollmentSecret: secret,
+            attr_reqs: [{name: 'hf.Registrar.Roles', optional: false}]
+        });
+        const x509Identity = {
+            credentials: {
+                certificate: enrollment.certificate,
+                privateKey: enrollment.key.toBytes(),
+            },
+            mspId: 'Org1MSP',
+            type: 'X.509',
+        };
+        await wallet.put(req.body.userId2, x509Identity);
+        console.log('Successfully registered and enrolled admin user "doctor1" and imported it into the wallet');
+        console.log(x509Identity)
+
+    } catch (error) {
+        console.error(`Failed to register user "doctor1": ${error}`);
+        process.exit(1);
+    }
+}
+
+
 app.listen(8080);
 // npx nodemon api_query.js
